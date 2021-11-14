a662dbcdce1d1cc261d2fb678b7c513ecc75fd36
saa27 <saakshi27050@gmail.com>
2021-11-14 12:41:41 +0530

    animations added

--- screens/CharacterDetailScreen.js
+++ screens/CharacterDetailScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Image, StyleSheet, Text, Animated } from 'react-native';
 import { SharedElement } from 'react-navigation-shared-element/build/v4';
-import { CHARACTERS } from '../data';
+import { Entypo } from '@expo/vector-icons';
+import * as Animatable from 'react-native-animatable';
 
 const CharacterDetailsScreen = (props) => {
     const post = props.navigation.getParam('post');
+
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 250,
+            delay: 500,
+            useNativeDriver: true,
+        }).start();
+    }, []);
 
     return (
         <View
@@ -14,7 +26,15 @@ const CharacterDetailsScreen = (props) => {
                 backgroundColor: `${post.bg}`,
             }}
         >
-            <View style={{ marginTop: 30 }}>
+            <Animated.View style={{ ...styles.icon, opacity }}>
+                <Entypo
+                    name="cross"
+                    size={40}
+                    color="black"
+                    onPress={() => props.navigation.goBack()}
+                />
+            </Animated.View>
+            <View style={{ marginTop: 10 }}>
                 <SharedElement id={post.id}>
                     <Image
                         source={{ uri: post.image }}
@@ -23,24 +43,36 @@ const CharacterDetailsScreen = (props) => {
                 </SharedElement>
             </View>
             <View style={styles.detailsCont}>
-                <View style={styles.nameCont}>
+                <Animatable.View
+                    animation="bounceIn"
+                    delay={500}
+                    style={styles.nameCont}
+                >
                     <Text style={styles.name}>{post.name}</Text>
-                </View>
+                </Animatable.View>
                 <View style={styles.bioCont}>
-                    <View style={styles.headingCont}>
+                    <Animatable.View
+                        animation="fadeInRight"
+                        delay={600}
+                        style={styles.headingCont}
+                    >
                         <Text style={styles.heading}>⚡ House:</Text>
                         <Text style={styles.heading}>⚡ Species:</Text>
                         <Text style={styles.heading}>⚡ Ancestry:</Text>
                         <Text style={styles.heading}>⚡ Actor:</Text>
                         <Text style={styles.heading}>⚡ Date of Birth:</Text>
-                    </View>
-                    <View style={styles.valueCont}>
+                    </Animatable.View>
+                    <Animatable.View
+                        animation="fadeInRight"
+                        delay={650}
+                        style={styles.valueCont}
+                    >
                         <Text style={styles.value}>{post.house}</Text>
                         <Text style={styles.value}>{post.species}</Text>
                         <Text style={styles.value}>{post.ancestry}</Text>
                         <Text style={styles.value}>{post.actor}</Text>
                         <Text style={styles.value}>{post.dateOfBirth}</Text>
-                    </View>
+                    </Animatable.View>
                 </View>
             </View>
         </View>
@@ -53,6 +85,10 @@ CharacterDetailsScreen.sharedElements = (route) => {
 };
 
 const styles = StyleSheet.create({
+    icon: {
+        alignSelf: 'flex-start',
+        margin: 10,
+    },
     harrypotter: {
         height: 110 * 2,
         width: 130 * 2,
@@ -61,9 +97,9 @@ const styles = StyleSheet.create({
     detailsCont: {
         marginTop: 40,
         height: 500,
-        backgroundColor: 'white',
-        borderTopRightRadius: 50,
-        borderTopLeftRadius: 50,
+        backgroundColor: 'black',
+        borderTopRightRadius: 35,
+        borderTopLeftRadius: 35,
         width: '100%',
     },
     nameCont: {
@@ -73,6 +109,7 @@ const styles = StyleSheet.create({
     name: {
         fontFamily: 'Parry',
         fontSize: 40,
+        color: '#ffb703',
     },
     bioCont: {
         marginTop: 30,
@@ -81,11 +118,13 @@ const styles = StyleSheet.create({
     },
     heading: {
         fontWeight: 'bold',
-        fontSize: 20
+        fontSize: 20,
+        color: 'white',
     },
     value: {
-        fontSize: 20
-    }
+        fontSize: 19.5,
+        color: 'white',
+    },
 });
 
 export default CharacterDetailsScreen;
